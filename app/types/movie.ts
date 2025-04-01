1b0f322dbd0aa4c4c903833f332745ece69339c9
David Nemes <nmsdvid@gmail.com>
2025-04-01 16:58:50 +0200

    🔧 Extended Movie Type

--- app/types/movie.ts
+++ app/types/movie.ts
@@ -1,4 +1,12 @@
 export interface Movie {
     Title: string
     imdbID: string
+    Poster: string
+    Year: string
+    Released: string
+    Genre: string
+    Runtime: string
+    Director: string
+    Actors: string
+    Plot: string
 }
